f7cdfe5215f4caeb45275943b659276085f46e56
Rafe12345 <rafechan27@gmail.com>
2024-11-01 16:43:20 +0800

    starss

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 const controls = new OrbitControls( camera, renderer.domElement );
@@ -23,6 +23,25 @@ scene.add(sphere);
 const light = new THREE.HemisphereLight( 0xffffff, 0x000000, 4 );
 light.position.set(0,1,0);
 scene.add( light );
+const axesHelper = new THREE.AxesHelper( 1000 );
+scene.add( axesHelper );
+function stars(){
+	let whd = Math.random() * (0.8- 0.1) + 0.1;
+	let x = Math.random() * (500 - -500) + -500;
+	let y = Math.random() * (400 - -150) +  -150;
+	let z = Math.random() * (250 - - 200) + - 200;
+	const geostars = new THREE.SphereGeometry(whd,44,22)
+	const material_stars = new THREE.MeshBasicMaterial({
+	})
+	const stars = new THREE.Mesh(geostars,material_stars)
+	stars.position.set(x,y,z)
+	scene.add(stars)
+}
+
+for(let i = 0; i<5000; i++){
+	stars()
+}
+
 function animate() {
 	sphere.rotation.y += 0.005;
 	sphere.rotation.x += 0.001;
