1746251758496aec07637fb77308abf15afe0761
Ranen <ranensim.yangyi@gmail.com>
2024-11-18 23:36:25 +0800

    updated lighting shadows

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ function stars() {
   let y = Math.random() * 600 - 300;
   let z = Math.random() * 600 - 300;
 
+  //Part 2 stars
   while (true) {
     if (x > 300 || x < -300) {
       break;
@@ -48,17 +49,19 @@ function stars() {
     }
   }
 
+  //part 3 stars
   const geostars = new THREE.SphereGeometry(whl);
-  const material_star = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const material_star = new THREE.MeshBasicMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geostars, material_star);
   star.position.set(x, y, z);
   scene.add(star);
 } //To close the stars function
+
 for (let i = 0; i < 5000; i++) {
   stars();
 }
 
-//Part 1 moon
+//Moon
 const moon_geometry = new THREE.SphereGeometry(4, 20, 20);
 const moon_texture = textureLoader.load("resources/moon.jpg");
 const moon_material = new THREE.MeshStandardMaterial({ map: moon_texture });
@@ -74,20 +77,37 @@ let angle = 0;
 const sunx = 25;
 const suny = 0;
 const sunz = 50;
-const light = new THREE.DirectionalLight(0xffffff, 1.1);
+const light = new THREE.DirectionalLight(0xffffff, 2);
 light.position.set(sunx, suny, sunz);
 scene.add(light);
 
+
 //Part 2 lighting
+renderer.shadowMap.enabled = true;
+light.castShadow = true;
+earthSphere.castShadow = true;
+moon.receiveShadow = true;
+light.shadow.mapSize.width = 1000; 
+light.shadow.mapSize.height = 1000;
+light.shadow.camera.left = -100; 
+light.shadow.camera.right = 100;
+light.shadow.camera.top = 100;
+light.shadow.camera.bottom = -100;
+
+
+
+//Part 3 lighting
 for (let x = -10; x <= 10; x += 10) {
   for (let z = -10; z <= 10; z += 10) {
     for (let y = -10; y <= 10; y += 10) {
-      const minilight = new THREE.DirectionalLight(0xffffff, 0.05);
+      const minilight = new THREE.PointLight(0xffffff, 110, 1000);
       minilight.position.set(sunx + x, suny + y, sunz + z);
       scene.add(minilight);
     }
   }
 }
+
+
 
 // Adding planet Mars
 const marsGeometry = new THREE.SphereGeometry(15, 100, 100);
